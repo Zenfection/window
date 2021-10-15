aa563964f06bf222e0f6158268cec05911792a98
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-15 23:33:06 +0700

    update

--- adobe_acrobat.bat
+++ adobe_acrobat.bat
@@ -1,29 +1,35 @@
 @echo off
+setlocal enabledelayedexpansion
 
-SET desktop=C:\Users\%username%\Desktop\
+SET desktop=C:\Users\%username%\Desktop
+SET ver=2100720099
 
-if not exist %desktop%\goodls.exe (
-    powershell wget "https://github.com/tanaikech/goodls/releases/download/v1.2.7/goodls_windows_amd64.exe" -O %desktop%\goodls.exe
+wmic datafile where name="C:\\Program Files (x86)\\Adobe\\Acrobat DC\\Acrobat\\Acrobat.exe" get Version /value > ver.txt
+type ver.txt | findstr /v "^$" > 123.txt
+set /p lastVer=<123.txt
+set /p lastVer=<123.txt
+set lastVer=%lastVer: =%
+set lastVer=%lastVer:.=%
+set lastVer=%lastVer:~8%
+set temp=%ver:~4,6%
+powershell rm ver.txt
+powershell rm 123.txt
+
+echo %lastVer% | findstr /C:%temp%>nul && (
+        ECHO Now App is installed newest !!
+        PAUSE
+        EXIT
+) || (
+    goto installUpdate
 )
 
-rem Cài file cài ??t t? gg drive
-cd %desktop%
-if not exist %desktop%\acrobat.zip (
-    powershell %desktop%\goodls.exe -u https://drive.google.com/file/d/1iQGThgTnCyKmRUIXsLm2ZPFEYNEMkLlC/view?usp=sharing -f acrobat.zip
-)
-powershell -Command "Expand-Archive acrobat.zip -DestinationPath acrobat"
+if not exist %desktop%\goodls.exe goto goodls
 
-start %desktop%\acrobat\Adobe_Acrobat_Pro_DC\Acrobat_DC_Web_WWMUI.exe
-"%desktop%\Adobe Acrobat\Adobe Acrobat\Setup.exe"
+if not exist %desktop%\acrobat.zip goto install
 
-rem T?i và ch?y file update
-wget https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/2100720099/AcrobatDCUpd2100720099.msp -O %desktop%\acrobatUpdate.msp
-%desktop%\acrobatUpdate.msp
+goto active
 
-rem Active ph?n m?m
-cd "%desktop%\acrobat\Adobe_Acrobat_Pro_DC\Activation"
-powershell -Command "Expand-Archive adobe.snr.patch.v2.0-painter.zip"
-adobe.snr.patch.v2.0-painter\adobe.snr.patch.v2.0-painter.exe
+
 
 rem Xoá các file không c?n thi?t
 cd %desktop%
@@ -32,4 +38,27 @@ if exist acrobat powershell rm -r acrobat
 if exist acrobatUpdate.msp powershell rm -r acrobatUpdate.msp
 if exist Adobe_Acrobat powershell rm -r Adobe_Acrobat
 
+goto end
+
+:goodls
+wget "https://github.com/tanaikech/goodls/releases/download/v1.2.7/goodls_windows_amd64.exe" -O %desktop%\goodls.exe
+
+:install
+cd %desktop%
+powershell %desktop%\goodls.exe -u https://drive.google.com/file/d/1iQGThgTnCyKmRUIXsLm2ZPFEYNEMkLlC/view?usp=sharing -f acrobat.zip
+powershell -Command "Expand-Archive acrobat.zip -DestinationPath acrobat"
+start \acrobat\Adobe_Acrobat_Pro_DC\Acrobat_DC_Web_WWMUI.exe   
+"\Adobe Acrobat\Adobe Acrobat\Setup.exe"
+
+:installUpdate
+cd %desktop%
+wget https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/%ver%/AcrobatDCUpd%ver%.msp -O acrobatUpdate.msp
+.\acrobatUpdate.msp 
+
+:active
+cd "%desktop%\acrobat\Adobe_Acrobat_Pro_DC\Activation"
+powershell -Command "Expand-Archive adobe.snr.patch.v2.0-painter.zip"
+adobe.snr.patch.v2.0-painter\adobe.snr.patch.v2.0-painter.exe
+
+:end
 PAUSE
