1034f3b11e1e63c24ce3a5a235c867556512cdcb
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-15 23:59:47 +0700

    Update evkey.bat

--- evkey.bat
+++ evkey.bat
@@ -15,12 +15,11 @@ rem Install EVkey
 cd %desktop%
 wget "https://github.com/lamquangminh/EVKey/releases/download/Release/EVKey.zip"
 powershell -Command "Expand-Archive EVKey.zip -DestinationPath C:\EVKey"
+start C:/EVkey/EVKey64.exe
 
 rem Delete all file not necessary
 if exist C:\EVKey\EVKey32.exe powershell rm -r C:\EVKey\EVKey32.exe
 if C:\EVKey\SHA256.txt powershell rm -r C:\EVKey\SHA256.txt
 if %desktop%\EVKey.zip powershell rm -r %desktop%\EVKey.zip
     
-rem Run Evkey 64bit
-start C:/EVkey/EVKey64.exe
 PAUSE
