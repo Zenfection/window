2a0bd918c0e44a92491f4581071c8ac3e76df9bc
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-15 19:28:26 +0700

    Update evkey.bat

--- evkey.bat
+++ evkey.bat
@@ -3,14 +3,14 @@
 SET desktop=C:\Users\%username%\Desktop\
 
 if not exist C:\EVKey mkdir C:\EVKey
-if not exist %desktop%\EVKey.zip (powershell rm -r %desktop%EVKey.zip)
+if exist %desktop%\EVKey.zip powershell rm -r %desktop%EVKey.zip
 if not exist C:\EVKey\evkau.exe (
     rem Install EVkey
     cd %desktop%
     wget "https://github.com/lamquangminh/EVKey/releases/download/Release/EVKey.zip"
     powershell -Command "Expand-Archive EVKey.zip -DestinationPath C:\EVKey"
 
-    rem Xoá nh?ng file không c?n thi?t
+    rem Delete all file not necessary
     if exist C:\EVKey\EVKey32.exe powershell rm -r C:\EVKey\EVKey32.exe
     if C:\EVKey\SHA256.txt powershell rm -r C:\EVKey\SHA256.txt
     if %desktop%\EVKey.zip powershell rm -r %desktop%\EVKey.zip
