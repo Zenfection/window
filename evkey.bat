f177f195be6c755e3eb3e809d77066d6af849bd7
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-15 19:35:38 +0700

    Update evkey.bat

--- evkey.bat
+++ evkey.bat
@@ -3,21 +3,24 @@
 SET desktop=C:\Users\%username%\Desktop\
 
 if not exist C:\EVKey mkdir C:\EVKey
-if exist %desktop%\EVKey.zip ( powershell rm -r %desktop%EVKey.zip )
-if not exist C:\EVKey\evkau.exe (
-    rem Install EVkey
-    cd %desktop%
-    wget "https://github.com/lamquangminh/EVKey/releases/download/Release/EVKey.zip"
-    powershell -Command "Expand-Archive EVKey.zip -DestinationPath C:\EVKey"
+if exist %desktop%\EVKey.zip ( del %desktop%EVKey.zip )
 
-    rem Delete all file not necessary
-    if exist C:\EVKey\EVKey32.exe powershell rm -r C:\EVKey\EVKey32.exe
-    if C:\EVKey\SHA256.txt powershell rm -r C:\EVKey\SHA256.txt
-    if %desktop%\EVKey.zip powershell rm -r %desktop%\EVKey.zip
-    
-    rem Run Evkey 64bit
-    start C:/EVkey/EVKey64.exe
-) else (
-    echo "EVkey is installed !!!"
+if exist C:\EVKey\evkau.exe (
+    echo EVKey installed !!!
+    pause
+    exit
 )
+
+rem Install EVkey
+cd %desktop%
+wget "https://github.com/lamquangminh/EVKey/releases/download/Release/EVKey.zip"
+powershell -Command "Expand-Archive EVKey.zip -DestinationPath C:\EVKey"
+
+rem Delete all file not necessary
+if exist C:\EVKey\EVKey32.exe powershell rm -r C:\EVKey\EVKey32.exe
+if C:\EVKey\SHA256.txt powershell rm -r C:\EVKey\SHA256.txt
+if %desktop%\EVKey.zip powershell rm -r %desktop%\EVKey.zip
+    
+rem Run Evkey 64bit
+start C:/EVkey/EVKey64.exe
 PAUSE
