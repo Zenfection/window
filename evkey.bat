1fbc5bdb8778f202c5e152b29edb7a1a6cf5544f
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-16 16:40:15 +0700

    Update evkey.bat

--- evkey.bat
+++ evkey.bat
@@ -5,8 +5,6 @@ SET cur=%cd%
 
 goto exist
 goto install
-goto delete
-goto run
 goto end
 
 :exist
@@ -22,15 +20,13 @@ cd "%desktop%"
 wget --no-check-certificate https://github.com/Zenfection/window/files/7357023/EVKey.zip -O EVKey.zip
 if not exist C:\EVKey mkdir C:\EVKey
 powershell -Command "Expand-Archive EVKey.zip -DestinationPath C:\EVKey"
+"C:/EVkey/EVKey64.exe"
 
 :delete
 cd "%desktop%"
 if exist EVKey.zip powershell rm -r EVKey.zip
 if exist EVKey powershell rm -r EVKey
 if exist "%cur%\evkey.bat" del "%cur%\evkey.bat"
-
-:run 
-C:/EVkey/EVKey64.exe
 
 :end
 pause
