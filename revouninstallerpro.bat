5d780d3326ec91c0f498b9e875afdab1133388d2
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-16 15:13:35 +0700

    Update revouninstallerpro.bat

--- revouninstallerpro.bat
+++ revouninstallerpro.bat
@@ -2,8 +2,6 @@
 
 SET desktop=C:\Users\%username%\Desktop
 
-
-sudo
 goto install
 goto active
 goto delete
@@ -11,16 +9,16 @@ goto end
 
 :install
 cd %desktop%
-curl https://download.revouninstaller.com/download/RevoUninProSetup.exe -O -L
-.\RevoUninProSetup.exe
+wget https://download.revouninstaller.com/download/RevoUninProSetup.exe -O revo.exe
+%desktop%\revo.exe
 
 
 :active
 cd %desktop%
-curl https://github.com/Zenfection/window/files/7353627/revouninstallerpro4.zip -O -L
+wget https://github.com/Zenfection/window/files/7353627/revouninstallerpro4.zip -O revouninstallerpro4.zip
 powershell -Command "Expand-Archive revouninstallerpro4.zip"
-del "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro\revouninstallerpro4.lic"
-move %desktop%\revouninstallerpro4\revouninstallerpro4.lic "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro\"
+sudo del /S /Q "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro\revouninstallerpro4.lic"
+sudo move %desktop%\revouninstallerpro4\revouninstallerpro4.lic "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro\"
 
 
 :delete
