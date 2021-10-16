dcf4dbec5658bab9175c0ce8d88f9e35ff2464e5
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-16 12:56:52 +0700

    Update revouninstallerpro.bat

--- revouninstallerpro.bat
+++ revouninstallerpro.bat
@@ -18,6 +18,7 @@ curl https://download.revouninstaller.com/download/RevoUninProSetup.exe -O -L
 cd %desktop%
 curl https://github.com/Zenfection/window/files/7353627/revouninstallerpro4.zip -O -L
 powershell -Command "Expand-Archive revouninstallerpro4.zip"
+powershell rm -r "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro\revouninstallerpro4.lic"
 move %desktop%\revouninstallerpro4\revouninstallerpro4.lic "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro\"
 
 
